489ef2af6887d48f76a78726bc4108af6b318185
slava <osslav@mail.com>
2022-06-13 22:37:30 +0700

    add class MainWindow

--- lab_3.pro
+++ lab_3.pro
@@ -18,6 +18,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+        mainwindow.cpp \
         themewidget.cpp \
         widgetchart.cpp \
         main.cpp
@@ -28,5 +29,6 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 HEADERS += \
+    mainwindow.h \
     themewidget.h \
     widgetchart.h
---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QWidget>
+#include <QFileSystemModel>
+#include <QTreeView>
+#include <QTableView>
+#include <QBoxLayout>
+#include "themewidget.h"
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+private slots:
+	void on_selectionChangedSlot(const QItemSelection &selected, const QItemSelection &deselected);
+
+public:
+	MainWindow(QWidget *parent = 0);
+	~MainWindow();
+private:
+	QFileSystemModel *fileModel;
+	QFileSystemModel *dirModel;
+	QTreeView *treeView;
+	QTableView *tableView;
+    ThemeWidget *themeWidget;
+    QVBoxLayout *tableLayout;
+    QHBoxLayout *tableButtonLayout;
+};
+
+#endif // MAINWINDOW_H
