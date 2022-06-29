d2f282794c768579d305be065bce38ddb333654f
slava <osslav@mail.com>
2022-06-29 21:30:06 +0700

    fix names in IOC container

--- mychart.cpp
+++ mychart.cpp
@@ -28,13 +28,13 @@
 //конструктор
 MyChart::MyChart()
 {
-    printer = IOCContainer::instance().GetttObject<IPrinterChart>();        //получаем принтер из контейнера
+    printer = IOCContainer::instance().Get_Object<IPrinterChart>();        //получаем принтер из контейнера
 }
 
 //функция обновления графика
 void MyChart::updateGraphic(bool notColoredChart, int startPosition, int length)
 {
-    printer = IOCContainer::instance().GetttObject<IPrinterChart>();        //получаем из контейнера принтер
+    printer = IOCContainer::instance().Get_Object<IPrinterChart>();        //получаем из контейнера принтер
     notColored = notColoredChart;                                           //обновляем поля класса, касающиеся свойств представления графика
     currentLenght = length;
     currentStartPosition = startPosition;
@@ -45,7 +45,7 @@ void MyChart::updateGraphic(bool notColoredChart, int startPosition, int length)
 //обновление данных графика
 bool MyChart::updateData(const QString& filePath)
 {
-    return IOCContainer::instance().GetttObject<IReadData>()->readData(m_dataList, filePath);   //получаем из контейнера ридер и сразу обновляем с его помощью данные
+    return IOCContainer::instance().Get_Object<IReadData>()->readData(m_dataList, filePath);   //получаем из контейнера ридер и сразу обновляем с его помощью данные
 }
 
 //функция обновления представления графика
